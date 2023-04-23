415bc28eb130462d5efc8d9256f710a909c3f5e9
Ilya Matsiushyn <matushin.97@gmail.com>
2023-04-23 18:16:00 +0300

    feat: add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,14 +4,32 @@ import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
-    const fileLoader = {
-            test: /\.(png|jpe?g|gif|woff2|woff)$/i,
-            use: [
-                {
-                    loader: 'file-loader',
-                },
-            ],
+    const babelLoader = {
+        test: /.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    ["i18next-extract", {
+                        locales: ['ru', 'en'],
+                        keyAsDefaultValue: true,
+                    }
+                    ],
+                ]
+            }
         }
+    }
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
 
     const svgLoader = {
         test: /\.svg$/,
@@ -47,6 +65,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoaders,
 
