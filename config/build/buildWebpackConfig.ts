ba217562ccf3e7c8d643876aafa137cfe379de38
Ilya Matsiushyn <matushin.97@gmail.com>
2023-04-23 19:19:21 +0300

    feat: add eslint, fix all files

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,13 +1,13 @@
-import webpack from "webpack";
-import {BuildOptions} from "./types/config";
-import path from "path";
-import {buildPlugins} from "./buildPlugins";
-import {buildLoaders} from "./buildLoaders";
-import {buildResolvers} from "./buildResolvers";
-import {buildDevServer} from "./buildDevServer";
+import webpack from 'webpack';
+import path from 'path';
+import { BuildOptions } from './types/config';
+import { buildPlugins } from './buildPlugins';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {paths, mode, isDev} = options
+    const { paths, mode, isDev } = options;
     return {
         mode,
         entry: paths.entry,
@@ -22,7 +22,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             rules: buildLoaders(options),
         },
         resolve: buildResolvers(options),
-        devtool: isDev ? "inline-source-map": undefined,
-        devServer: isDev ? buildDevServer(options): undefined
-    }
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+    };
 }
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -6,13 +6,14 @@ declare module '*.scss' {
     export = classNames;
 }
 
-declare module "*.svg";
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.svg" {
-    import React from "react";
+declare module '*.svg';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg' {
+    import React from 'react';
+
     const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
     export default SVG;
 }
 
-declare const __IS_DEV__ : boolean
+declare const __IS_DEV__ : boolean;
