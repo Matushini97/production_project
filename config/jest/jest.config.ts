c78c2a146e820641d8f97cb13c2c9885d8430af2
Ilya Matsiushyn <matushin.97@gmail.com>
2023-04-30 00:18:58 +0300

    feat: add react testing library, add tests for btn sidebar

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 export default {
 
     clearMocks: true,
@@ -16,9 +18,17 @@ export default {
     moduleDirectories: [
         'node_modules',
     ],
+    modulePaths: [
+        '<rootDir>src',
+    ],
     testMatch: [
         // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
     ],
     rootDir: '../../',
+    setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+    moduleNameMapper: {
+        '\\.s?css$': 'identity-obj-proxy',
+        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    },
 };
