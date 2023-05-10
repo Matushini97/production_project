36d4cac0a555ee03b73acf486afc99838471816d
Ilya Matsiushyn <matushin.97@gmail.com>
2023-05-10 18:21:23 +0300

    feat: continue with storybook

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Button, ThemeButton } from './Button';
+import 'app/styles/index.scss';
 
 const meta = {
     title: 'shared/Button',
