faf73ab4dbec34e7ca4c148cf7ee31f7cb22924c
Ilya Matsiushyn <matushin.97@gmail.com>
2023-05-14 22:50:41 +0300

    fix: add storybook unversioned files

---
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -0,0 +1,27 @@
+import type { StoryObj } from '@storybook/react';
+import 'app/styles/index.scss';
+import { Theme } from 'app/providers/ThemeProviders';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import MainPage from './MainPage';
+
+const meta = {
+    title: 'pages/MainPage',
+    component: MainPage,
+    tags: ['autodocs'],
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Normal: Story = {
+    args: {},
+};
+
+export const Dark: Story = {
+    args: {},
+    decorators: [ThemeDecorator(Theme.DARK)],
+};
