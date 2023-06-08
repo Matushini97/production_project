e6fb9182555fa87c0e2c5fead6377ddc04eace2c
Ilya Matsiushyn <matushin.97@gmail.com>
2023-06-08 15:25:58 +0300

    feat: add login module

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.ts
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.ts
@@ -1,5 +1,8 @@
 import type { StoryObj } from '@storybook/react';
 import 'app/styles/index.scss';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProviders';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDcorator';
 import { LoginForm } from './LoginForm';
 
 const meta = {
@@ -18,4 +21,20 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
     args: {
     },
+};
+
+export const withError: Story = {
+    args: {
+    },
+    decorators: [StoreDecorator({
+        loginForm: { username: '123', password: 'asd', error: 'ERROR' },
+    })],
+};
+
+export const Loading: Story = {
+    args: {
+    },
+    decorators: [StoreDecorator({
+        loginForm: { isLoading: true },
+    })],
 };
